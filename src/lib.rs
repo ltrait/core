a51b1cb3d134095ea9ceef01b883c73dfda94deb
satler <satler@satler.dev>
2025-03-07 11:35:09 +0900

    feat: log

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ pub use async_trait;
 pub use color_eyre;
 pub use tokio_stream;
 
+pub use tracing::Level;
+use tracing_log::LogTracer;
+use tracing_subscriber::fmt::format::FmtSpan;
+
 pub mod action;
 pub mod filter;
 pub mod generator;
@@ -14,3 +18,27 @@ pub use crate::action::Action;
 pub use crate::filter::Filter;
 pub use crate::launcher::Launcher;
 pub use crate::sorter::Sorter;
+
+use color_eyre::eyre::Result;
+
+fn init_subscriber_with_level(level: Level) {
+    tracing_subscriber::fmt()
+        .with_max_level(level) // set recorded log level
+        .with_span_events(FmtSpan::ACTIVE) // enable record span timing
+        .init();
+}
+
+/// Install color_eyre and setup tracing(with tracing-log)
+/// ```
+/// use ltrait::{Level, setup};
+///
+/// let _ = setup(Level::TRACE);
+/// ```
+pub fn setup(log_level: Level) -> Result<()> {
+    color_eyre::install()?;
+
+    init_subscriber_with_level(log_level);
+    LogTracer::init()?;
+
+    Ok(())
+}
