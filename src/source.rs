d3da9906b5c6abcecca17f51fba42a0ea1d41ca7
satler <satler@satler.dev>
2025-03-14 20:03:51 +0900

    feat: add `ltrait::source::from_iter`

--- src/source.rs
+++ src/source.rs
@@ -1,3 +1,12 @@
 use std::pin::Pin;
 
 pub type Source<'a, T> = Pin<Box<dyn tokio_stream::Stream<Item = T> + Send + 'a>>;
+
+fn from_iter<'a, T, Iter>(
+    iter: impl std::iter::IntoIterator<Item = T, IntoIter = Iter>,
+) -> Source<'a, T>
+where
+    Iter: std::iter::Iterator<Item = T> + Send + 'a,
+{
+    Box::pin(tokio_stream::iter(iter))
+}
