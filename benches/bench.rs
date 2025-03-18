531c0c23c63cf39f790684aca19a3e71d7c28c84
satler <satler@satler.dev>
2025-03-18 20:09:13 +0900

    feat: add benchmarks

---
+++ benches/bench.rs
@@ -0,0 +1,84 @@
+#[path = "../tests/dummyui.rs"]
+mod dummyui;
+
+use color_eyre::eyre::Result;
+use criterion::{Criterion, criterion_group, criterion_main};
+
+use dummyui::DummyUI;
+use ltrait::{Launcher, filter::ClosureFilter, sorter::ClosureSorter, source::from_iter};
+
+use std::convert::identity;
+
+use tokio::runtime::Runtime;
+
+async fn simple_source_a() -> Result<()> {
+    let launcher = Launcher::default()
+        .add_source(from_iter(0..500_000), identity)
+        .batch_size(1000)
+        .set_ui(DummyUI::new(|_: &()| {}), |_| ());
+
+    launcher.run().await?;
+
+    Ok(())
+}
+
+async fn simple_filter_a() -> Result<()> {
+    let launcher = Launcher::default()
+        .add_source(from_iter(0..500_000), identity)
+        .batch_size(1000)
+        .add_filter(ClosureFilter::new(|_: &i32, _| true), |&c: &i32| c)
+        .add_filter(ClosureFilter::new(|_: &i32, _| true), |&c: &i32| c)
+        .add_filter(ClosureFilter::new(|_: &i32, _| true), |&c: &i32| c)
+        .set_ui(DummyUI::new(|_: &()| {}), |_| ());
+
+    launcher.run().await?;
+
+    Ok(())
+}
+
+async fn simple_sorter_a() -> Result<()> {
+    let launcher = Launcher::default()
+        .add_source(from_iter(0..500_000), identity)
+        .batch_size(1000)
+        .add_sorter(
+            ClosureSorter::new(|_: &i32, _: &i32, _| std::cmp::Ordering::Equal),
+            |&c: &i32| c,
+        )
+        .add_sorter(
+            ClosureSorter::new(|_: &i32, _: &i32, _| std::cmp::Ordering::Equal),
+            |&c: &i32| c,
+        )
+        .add_sorter(
+            ClosureSorter::new(|_: &i32, _: &i32, _| std::cmp::Ordering::Equal),
+            |&c: &i32| c,
+        )
+        .set_ui(DummyUI::new(|_: &()| {}), |_| ());
+
+    launcher.run().await?;
+
+    Ok(())
+}
+
+fn simple_source(c: &mut Criterion) {
+    c.bench_function("500,000 Items, batch_size = 1000", |b| {
+        b.to_async(Runtime::new().unwrap())
+            .iter(|| simple_source_a());
+    });
+}
+
+fn simple_filter(c: &mut Criterion) {
+    c.bench_function("50,000 Items, batch_size = 1000, 3 filters", |b| {
+        b.to_async(Runtime::new().unwrap())
+            .iter(|| simple_filter_a());
+    });
+}
+
+fn simple_sorter(c: &mut Criterion) {
+    c.bench_function("500,000 Items, batch_size = 1000, 3 sorters", |b| {
+        b.to_async(Runtime::new().unwrap())
+            .iter(|| simple_sorter_a());
+    });
+}
+
+criterion_group!(benches, simple_source, simple_filter, simple_sorter);
+criterion_main!(benches);
