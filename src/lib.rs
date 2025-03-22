1188c8c9b03192f730eab38066baad179189d1d0
satler <satler@satler.dev>
2025-03-22 10:02:27 +0900

    feat: separate of log functions into the feature flag(`log`)

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ pub use async_trait;
 pub use color_eyre;
 pub use tokio_stream;
 
+#[cfg(feature = "log")]
 pub use tracing::Level;
-use tracing_subscriber::fmt::format::FmtSpan;
 
 pub mod action;
 pub mod filter;
@@ -21,13 +21,35 @@ pub use crate::sorter::Sorter;
 pub use crate::source::Source;
 pub use crate::ui::UI;
 
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{OptionExt, Result};
 
-fn init_subscriber_with_level(level: Level) {
+#[cfg(feature = "log")]
+fn init_subscriber_with_level(level: Level) -> Result<()> {
+    use tracing_subscriber::fmt::format::FmtSpan;
+
+    fn db_dir() -> Option<std::path::PathBuf> {
+        let path = dirs::data_dir().map(|p| p.join("ltrait/log"));
+
+        if let Some(parent) = path.as_ref().and_then(|p| p.parent()) {
+            std::fs::create_dir_all(parent).ok()?;
+        }
+
+        path
+    }
+
+    let file_appender = tracing_appender::rolling::hourly(
+        db_dir().ok_or_eyre("failed to get log dir")?,
+        "core.log",
+    );
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
     tracing_subscriber::fmt()
+        .with_writer(non_blocking)
         .with_max_level(level) // set recorded log level
         .with_span_events(FmtSpan::ACTIVE) // enable record span timing
         .init();
+
+    Ok(())
 }
 
 /// Install color_eyre and setup tracing(with tracing-log)
@@ -39,7 +61,6 @@ fn init_subscriber_with_level(level: Level) {
 pub fn setup(log_level: Level) -> Result<()> {
     color_eyre::install()?;
 
-    init_subscriber_with_level(log_level);
-
-    Ok(())
+    #[cfg(feature = "log")]
+    init_subscriber_with_level(log_level)
 }
